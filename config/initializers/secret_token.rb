03c166bc312a4a9a4461bbcee11e72fe755771fa
John Lassiter <mecrobmarmit@yahoo.com>
2011-04-19 23:56:17 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LemuredWebsite::Application.config.secret_token = '6920e36f17347fab36b9708018809f622df2f08ce3b911ee4531454f197435aba89062e719fd87424979e83abb00884b983b09384f2b050b89fcc78880bb3d1a'
